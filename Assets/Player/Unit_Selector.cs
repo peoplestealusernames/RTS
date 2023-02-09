cd7eb40dcd8232b61e2d06f5b4ca6c44369d71f4
10K <bretthoganpr@gmail.com>
2023-02-09 13:14:53 -0600

    Added Nav agent and move on click raycast

--- Assets/Player/Unit_Selector.cs
+++ Assets/Player/Unit_Selector.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Unit_Selector : MonoBehaviour
 {
     public Selected_Units selected;
+    public Camera Cam;
 
     void Update()
     {
@@ -14,7 +16,17 @@ public class Unit_Selector : MonoBehaviour
         }
         else if (Input.GetMouseButtonDown(1))
         {
-            //TODO: Move
+            int layerMask = 1 << 0;
+
+            RaycastHit hit;
+            if (Physics.Raycast(Cam.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, layerMask))
+            {
+                foreach (Transform unit in selected.Get())
+                {
+                    NavMeshAgent _Nav = unit.GetComponent<NavMeshAgent>();
+                    _Nav.destination = hit.point;
+                }
+            }
         }
     }
 }
