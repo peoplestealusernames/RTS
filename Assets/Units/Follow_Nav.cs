30afdf492d1c93583edc94caac412055b9d40deb
10K <bretthoganpr@gmail.com>
2023-02-10 13:48:07 -0600

    Follow_Nav: WIP Change for later

--- Assets/Units/Follow_Nav.cs
+++ Assets/Units/Follow_Nav.cs
@@ -9,7 +9,10 @@ public class Follow_Nav : MonoBehaviour
     public NavMeshAgent Nav;
     public float SmoothingT = 2f;
 
+    public float MaxRotSpeed = 20f;
+
     private Vector3 vel = Vector3.zero;
+    private Vector3 aVel = Vector3.zero;
 
     private void Start()
     {
@@ -20,9 +23,13 @@ public class Follow_Nav : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 _Level = Nav.nextPosition;
-        _Level.y = Follower.position.y;
-        Follower.LookAt(_Level);
+        Vector3 _Dif = Follower.transform.InverseTransformPoint(_Level);
 
-        Follower.position = Vector3.SmoothDamp(Follower.position, _Level, ref vel, SmoothingT);
+        if (_Dif.magnitude > 1)
+        {
+            Quaternion _Dir = Quaternion.LookRotation(_Dif);
+            Follower.rotation = Quaternion.RotateTowards(Follower.rotation, _Dir, MaxRotSpeed);
+            Follower.position = Vector3.SmoothDamp(Follower.position, _Level, ref vel, SmoothingT);
+        }
     }
 }
