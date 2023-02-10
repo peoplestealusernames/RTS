0b66b7fcacd4437577f8fc5c7e1640c83056fa25
10K <bretthoganpr@gmail.com>
2023-02-09 21:54:19 -0600

    Rover: Movement update

--- Assets/Units/Follow_Nav.cs
+++ Assets/Units/Follow_Nav.cs
@@ -7,14 +7,22 @@ public class Follow_Nav : MonoBehaviour
 {
     public Transform Follower;
     public NavMeshAgent Nav;
-    public float SmoothingT = 10f;
+    public float SmoothingT = 2f;
+
+    private Vector3 vel = Vector3.zero;
+
+    private void Start()
+    {
+        Nav.updatePosition = false;
+        Nav.updateRotation = false;
+    }
 
     private void FixedUpdate()
     {
-        Vector3 _Level = Nav.transform.position;
+        Vector3 _Level = Nav.nextPosition;
         _Level.y = Follower.position.y;
         Follower.LookAt(_Level);
 
-        Follower.position = Vector3.Lerp(Follower.position, _Level, 1 / SmoothingT);
+        Follower.position = Vector3.SmoothDamp(Follower.position, _Level, ref vel, SmoothingT);
     }
 }
