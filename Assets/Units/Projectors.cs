75b6cd46771f7fbfa16d48b7757edde6367c6840
10K <bretthoganpr@gmail.com>
2023-02-25 16:59:18 -0600

    added projector refrence holder

---
+++ Assets/Units/Projectors.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+public class Projectors : MonoBehaviour
+{
+    public DecalProjector Range;
+    public DecalProjector Intel;
+    public DecalProjector AA;
+}
